84412c52da3db8c4f2ee38f9c631f16f51c5270b
shoneyj <shoney.john@outlook.com>
2024-03-11 01:11:58 +0100

    initial

---
+++ AuthContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcoreApp
+{
+    public class AuthContext : DbContext
+    {
+        private readonly string _connectionString = "Host=localhost;Port=5432;Username=admin;Password=admin;Database=writeonce";
+        //public AuthContext(string connectionString)
+        //{
+        //    _connectionString = connectionString;
+        //}
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+     => options.UseNpgsql(_connectionString);
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using EFcoreApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
+
+namespace EFcoreApp
+{
+    public class Program {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("entity framework testing");
+            var services = new ServiceCollection();
+            var configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddJsonFile("appsettings.json");
+
+            var configuration = configurationBuilder.Build();
+            services.AddSingleton<IConfiguration>(configuration);
+            var connectionString = configuration.GetConnectionString("defaultConnection");
+            services.AddDbContext<AuthContext>(options => { options.UseNpgsql(connectionString); });
+            var db = new AuthContext();
+            Console.WriteLine($"App Main ");
+        }
+
+        }
+
+}
+      
